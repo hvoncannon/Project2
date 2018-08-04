790243f671643ba6fd3c2a5fc280f9615c53daa7
hvoncannon <hayden.voncannon@gmail.com>
2018-08-04 13:24:19 -0700

    changed html route for new post to just show form, does not need to load all existing posts

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,16 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/post", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("post", {
-        title: "Post.it",
-        msg: "Post Creation",
-        examples: dbExamples
-      });
+module.exports = function (app) {
+  // Load Post Page
+  app.get("/post", function (req, res) {
+    res.render("post", {
+      title: "Post.it",
+      msg: "Post Creation",
     });
   });
 
-  app.get("/author", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/author", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("author", {
         title: "Post.it",
         msg: "Author Creation",
@@ -22,15 +19,15 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/category", function(req, res) {
+  app.get("/category", function (req, res) {
     res.render("category", {
       msg: "Post a new category!"
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -38,7 +35,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
