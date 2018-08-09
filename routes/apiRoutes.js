8e1c258b87a14847c4633aa89e8aada12fe73427
Jonathan Mai <h.jmai2791@gmail.com>
2018-08-08 20:06:21 -0700

    clean up code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,6 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    // db.Example.create(req.body).then(function(dbExample) {
-    //   res.json(dbExample);
-    // });
-  });
-
   app.post("/new/post", function(req, res) {
     console.log(req.body);
     db.Post.create({
@@ -47,12 +33,5 @@ module.exports = function(app) {
         PostId: req.body.id
       });
     }
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
   });
 };
