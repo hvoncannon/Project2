551dd2e6b405db505b44f2537f60a22560693564
hvoncannon <hayden.voncannon@gmail.com>
2018-08-09 21:39:31 -0700

    wired up voting buttons

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  app.post("/new/post", function(req, res) {
+module.exports = function (app) {
+  app.post("/new/post", function (req, res) {
     console.log(req.body);
     db.Post.create({
       title: req.body.title,
@@ -10,16 +10,16 @@ module.exports = function(app) {
       upvotes: 0,
       downvotes: 0,
       UserId: req.user.id
-    }).then(function(dbPost){
+    }).then(function (dbPost) {
       res.json(dbPost);
     });
   });
 
-  app.post("/new/category", function(req, res) {
+  app.post("/new/category", function (req, res) {
     db.Categories.findOrCreate({
       where: { name: req.body.categoryName },
-      defaults: {name: req.body.categoryName, description: req.body.categoryDescription}
-    }).then(function(dbCategories) {
+      defaults: { name: req.body.categoryName, description: req.body.categoryDescription }
+    }).then(function (dbCategories) {
       res.json(dbCategories);
       // isNew = result[1];
       // console.log(isNew);
@@ -27,15 +27,37 @@ module.exports = function(app) {
     });
   });
 
-  app.post("/new/comment", function(req, res) {
+  app.post("/new/comment", function (req, res) {
     console.log(req.params);
-    if(req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
       db.Comment.create({
         text: req.body.text,
         UserId: req.user.id,
         PostId: req.body.id
-      }).then(function(dbComment) {
+      }).then(function (dbComment) {
         res.json(dbComment);
+      });
+    }
+  });
+
+  app.post("/upvote", function (req, res) {
+    if (req.isAuthenticated()) {
+      db.Post.increment(
+        { upvotes: 1 },
+        { where: { id: req.body.id } }
+      ).then(function (rowsUpdated) {
+        res.json(rowsUpdated);
+      });
+    }
+  });
+
+  app.post("/downvote", function (req, res) {
+    if (req.isAuthenticated()) {
+      db.Post.increment(
+        { downvotes: 1 },
+        { where: { id: req.body.id } }
+      ).then(function (rowsUpdated) {
+        res.json(rowsUpdated);
       });
     }
   });
